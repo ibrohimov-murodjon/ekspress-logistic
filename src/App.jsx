22b5b7b33ff26e666dfd17c3c5e41403daffb3c8
ibrohimov-murodjon <murodjon.ibrohimov.rishton@gmail.com>
2023-10-05 21:13:12 +0500

    deployed to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { Hero, About, Advantages, Team, Location } from "./sections";
-import { Header, Navbar } from "./components";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeLayout from "./layout/HomeLayout";
+import { Home, Premium, Error } from "./pages";
+import { ErrorElement } from "./components";
 
 export default function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+          // loader: landingLoader,
+          errorElement: <ErrorElement />,
+        },
+        {
+          path: "/premium",
+          element: <Premium />,
+          errorElement: <Error />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-      <Header />
-      <Navbar />
-      <Hero />
-      <About />
-      <Advantages />
-      <Team />
-      <Location />
+      <RouterProvider router={routes} />
     </>
   );
 }
--- src/components/ErrorElement.jsx
+++ src/components/ErrorElement.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 function ErrorElement() {
+  const error = useRouteError();
   return <h4 className="font-bold text-4xl">There was an error...</h4>;
 }
 
